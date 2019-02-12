bacc633800d34b98967bdefb13e3fa48bd0cb231
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2019-02-12 14:49:18 +0000

    MAde function for contrast stretching

--- src/Photoshop.java
+++ src/Photoshop.java
@@ -1,3 +1,4 @@
+
 /*
 CS-255 Getting started code for the assignment
 I do not give you permission to post this code online
@@ -11,8 +12,8 @@ Do not use JavaFX functions or other libraries to do the main parts of the assig
 All of those functions must be written by yourself
 You may use libraries to achieve a better GUI
 */
-import java.io.FileInputStream; 
-import java.io.FileNotFoundException; 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -23,39 +24,44 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;  
+import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.PixelReader;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class Photoshop extends Application 
+public class Photoshop extends Application
 {
-    private static final int BYTE_LIMIT = 256; 
-    
-    private int[] gammaLookupTable;
+	private static final int BYTE_LIMIT = 256;
 
-    private double gammaValue;
-    
-    public Photoshop()
-    {
-        gammaValue = 1.0;
-        computeGammaLookUpTable();
-    }
-    
-    @Override
-    public void start(Stage stage) throws FileNotFoundException 
-    {
+	private int[] gammaLookupTable;
+
+	private double gammaValue;
+	
+	private int r1 = 50;
+	private int s1 = 20;
+	private int r2 = 200;
+	private int s2 = 230;
+
+	public Photoshop()
+	{
+		gammaValue = 1.0;
+		computeGammaLookUpTable();
+	}
+
+	@Override
+	public void start(Stage stage) throws FileNotFoundException
+	{
 		stage.setTitle("Photoshop");
 
-		//Read the image
-		Image image = new Image(new FileInputStream("raytrace.jpg"));  
+		// Read the image
+		Image image = new Image(new FileInputStream("raytrace.jpg"));
 
-		//Create the graphical view of the image
-		ImageView imageView = new ImageView(image); 
-		
-		//Create the simple GUI
+		// Create the graphical view of the image
+		ImageView imageView = new ImageView(image);
+
+		// Create the simple GUI
 		Button invert_button = new Button("Invert");
 		Button gamma_button = new Button("Gamma Correct");
 		Button contrast_button = new Button("Contrast Stretching");
@@ -65,219 +71,255 @@ public class Photoshop extends Application
 		Button saveGammaButton = new Button("Save Gamma Value");
 		Label gammaInputLabel = new Label("Gamma value:");
 		TextField gammaInput = new TextField();
-        
-		//Add all the event handlers (this is a minimal GUI - you may try to do better)
-		invert_button.setOnAction(new EventHandler<ActionEvent>() 
+
+		// Add all the event handlers (this is a minimal GUI - you may try to do
+		// better)
+		invert_button.setOnAction(new EventHandler<ActionEvent>()
 		{
-            @Override
-            public void handle(ActionEvent event) 
-            {
-                System.out.println("Invert");
-				//At this point, "image" will be the original image
-				//imageView is the graphical representation of an image
-				//imageView.getImage() is the currently displayed image
-				
-				//Let's invert the currently displayed image by calling the invert function later in the code
-				Image inverted_image=imageInverter(imageView.getImage());
-				//Update the GUI so the new image is displayed
+			@Override
+			public void handle(ActionEvent event)
+			{
+				System.out.println("Invert");
+				// At this point, "image" will be the original image
+				// imageView is the graphical representation of an image
+				// imageView.getImage() is the currently displayed image
+
+				// Let's invert the currently displayed image by calling the
+				// invert function later in the code
+				Image inverted_image = imageInverter(imageView.getImage());
+				// Update the GUI so the new image is displayed
 				imageView.setImage(inverted_image);
-            }
-        });
+			}
+		});
 
-		gamma_button.setOnAction(new EventHandler<ActionEvent>() 
+		gamma_button.setOnAction(new EventHandler<ActionEvent>()
 		{
-            @Override
-            public void handle(ActionEvent event) 
-            {
-                System.out.println("Gamma Correction");
-                
-                long start = System.currentTimeMillis();
-                Image correctedImage = gammaCorrecter(image);
-                //Image correctedImage = gammaCorrecter(image, Double.parseDouble(gammaInput.getText()));
-                imageView.setImage(correctedImage);
-                System.out.println(System.currentTimeMillis()-start);
-            }
-        });
+			@Override
+			public void handle(ActionEvent event)
+			{
+				System.out.println("Gamma Correction");
 
-		contrast_button.setOnAction(new EventHandler<ActionEvent>() 
+				long start = System.currentTimeMillis();
+				Image correctedImage = gammaCorrecter(image);
+				// Image correctedImage = gammaCorrecter(image,
+				// Double.parseDouble(gammaInput.getText()));
+				imageView.setImage(correctedImage);
+				System.out.println(System.currentTimeMillis() - start);
+			}
+		});
+
+		contrast_button.setOnAction(new EventHandler<ActionEvent>()
 		{
-            @Override
-            public void handle(ActionEvent event) 
-            {
-                System.out.println("Contrast Stretching");
-            }
-        });
-		
-		histogram_button.setOnAction(new EventHandler<ActionEvent>() 
+			@Override
+			public void handle(ActionEvent event)
+			{
+				System.out.println("Contrast Stretching");
+			}
+		});
+
+		histogram_button.setOnAction(new EventHandler<ActionEvent>()
 		{
-            @Override
-            public void handle(ActionEvent event) 
-            {
-                System.out.println("Histogram");
-            }
-        });
-		
-		cc_button.setOnAction(new EventHandler<ActionEvent>() 
+			@Override
+			public void handle(ActionEvent event)
+			{
+				System.out.println("Histogram");
+			}
+		});
+
+		cc_button.setOnAction(new EventHandler<ActionEvent>()
 		{
-            @Override
-            public void handle(ActionEvent event) 
-            {
-                System.out.println("Cross Correlation");
-            }
-        });
-		
+			@Override
+			public void handle(ActionEvent event)
+			{
+				System.out.println("Cross Correlation");
+			}
+		});
+
 		resetButton.setOnAction(new EventHandler<ActionEvent>()
 		{
-		    @Override
-		    public void handle(ActionEvent event)
-		    {
-		        imageView.setImage(image);
-		    }
+			@Override
+			public void handle(ActionEvent event)
+			{
+				imageView.setImage(image);
+			}
 		});
-		
+
 		saveGammaButton.setOnAction(new EventHandler<ActionEvent>()
 		{
-		    @Override
-		    public void handle(ActionEvent event)
-		    {
-		        String gammaText = gammaInput.getText();
-		        if(!gammaText.equals(""))
-		        {
-		            gammaValue = Double.parseDouble(gammaInput.getText());
-	                computeGammaLookUpTable();
-		        }
-		    }
+			@Override
+			public void handle(ActionEvent event)
+			{
+				String gammaText = gammaInput.getText();
+				if (!gammaText.equals(""))
+				{
+					gammaValue = Double.parseDouble(gammaInput.getText());
+					computeGammaLookUpTable();
+				}
+			}
 		});
-		
-		//Using a flow pane
+
+		// Using a flow pane
 		BorderPane root = new BorderPane();
-		
+
 		FlowPane topElements = new FlowPane();
-		
-		//Gaps between buttons
+
+		// Gaps between buttons
 		topElements.setVgap(10);
 		topElements.setHgap(5);
 
-		//Add all the buttons and the image for the GUI
-		topElements.getChildren().addAll(invert_button, gamma_button, contrast_button,
-		    histogram_button, cc_button, resetButton, gammaInputLabel, gammaInput,
-		    saveGammaButton);
-		
+		// Add all the buttons and the image for the GUI
+		topElements.getChildren().addAll(invert_button, gamma_button, contrast_button, histogram_button, cc_button,
+				resetButton, gammaInputLabel, gammaInput, saveGammaButton);
+
 		root.setTop(topElements);
 		root.setCenter(imageView);
 
-		//Display to user
-        Scene scene = new Scene(root, 1024, 768);
-        stage.setScene(scene);
-        stage.show();
-    }
+		// Display to user
+		Scene scene = new Scene(root, 1024, 768);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-	//Example function of invert
-	public Image imageInverter(Image image) 
+	// Example function of invert
+	public Image imageInverter(Image image)
 	{
-		//Find the width and height of the image to be process
-		int width = (int)image.getWidth();
-        int height = (int)image.getHeight();
-		//Create a new image of that width and height
+		// Find the width and height of the image to be process
+		int width = (int) image.getWidth();
+		int height = (int) image.getHeight();
+		// Create a new image of that width and height
 		WritableImage inverted_image = new WritableImage(width, height);
-		//Get an interface to write to that image memory
+		// Get an interface to write to that image memory
 		PixelWriter inverted_image_writer = inverted_image.getPixelWriter();
-		//Get an interface to read from the original image passed as the parameter to the function
-		PixelReader image_reader=image.getPixelReader();
-		
-		//Iterate over all pixels
-		for(int y = 0; y < height; y++) {
-			for(int x = 0; x < width; x++) {
-				//For each pixel, get the colour
+		// Get an interface to read from the original image passed as the
+		// parameter to the function
+		PixelReader image_reader = image.getPixelReader();
+
+		// Iterate over all pixels
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				// For each pixel, get the colour
 				Color color = image_reader.getColor(x, y);
-				//Do something (in this case invert) - the getColor function returns colours as 0..1 doubles (we could multiply by 255 if we want 0-255 colours)
-				color=Color.color(1.0-color.getRed(), 1.0-color.getGreen(), 1.0-color.getBlue());
-				//Note: for gamma correction you may not need the divide by 255 since getColor already returns 0-1, nor may you need multiply by 255 since the Color.color function consumes 0-1 doubles.
-				
-				//Apply the new colour
+				// Do something (in this case invert) - the getColor function
+				// returns colours as 0..1 doubles (we could multiply by 255 if
+				// we want 0-255 colours)
+				color = Color.color(1.0 - color.getRed(), 1.0 - color.getGreen(), 1.0 - color.getBlue());
+				// Note: for gamma correction you may not need the divide by 255
+				// since getColor already returns 0-1, nor may you need multiply
+				// by 255 since the Color.color function consumes 0-1 doubles.
+
+				// Apply the new colour
 				inverted_image_writer.setColor(x, y, color);
 			}
 		}
 		return inverted_image;
 	}
-	
+
 	private void computeGammaLookUpTable()
 	{
-	    gammaLookupTable = new int[BYTE_LIMIT];
-	    for(int i=0; i < BYTE_LIMIT; i++)
-	    {
-	        int correctedValue = (int)(255*Math.pow(i/255.0, 1.0/gammaValue));
-	        gammaLookupTable[i] = correctedValue;
-	    }
+		gammaLookupTable = new int[BYTE_LIMIT];
+		for (int i = 0; i < BYTE_LIMIT; i++)
+		{
+			int correctedValue = (int) (255 * Math.pow(i / 255.0, 1.0 / gammaValue));
+			gammaLookupTable[i] = correctedValue;
+		}
 	}
-	
+
 	private int getCorrectedValue(double colour)
 	{
-	    int integerColour = (int)(colour*255);
-	    return gammaLookupTable[integerColour];
+		int integerColour = (int) (colour * 255);
+		return gammaLookupTable[integerColour];
 	}
-	
+
 	public Image gammaCorrecter(Image image)
 	{
-		int width = (int)image.getWidth();
-		int height = (int)image.getHeight();
-		
+		int width = (int) image.getWidth();
+		int height = (int) image.getHeight();
+
 		WritableImage correctedImage = new WritableImage(width, height);
-		
+
 		PixelWriter correctedImageWriter = correctedImage.getPixelWriter();
 		PixelReader imageReader = image.getPixelReader();
-		
-		for(int y = 0; y < height; y++)
+
+		for (int y = 0; y < height; y++)
 		{
-			for(int x = 0; x < width; x++)
+			for (int x = 0; x < width; x++)
 			{
 				Color pixelColor = imageReader.getColor(x, y);
-				
+
 				int correctedRed = getCorrectedValue(pixelColor.getRed());
 				int correctedGreen = getCorrectedValue(pixelColor.getGreen());
 				int correctedBlue = getCorrectedValue(pixelColor.getBlue());
-				
+
 				Color correctedColor = Color.rgb(correctedRed, correctedGreen, correctedBlue);
-				
+
 				correctedImageWriter.setColor(x, y, correctedColor);
 			}
 		}
-		
+
+		return correctedImage;
+	}
+
+	public Image gammaCorrecter(Image image, double gammaValue)
+	{
+		int width = (int) image.getWidth();
+		int height = (int) image.getHeight();
+
+		WritableImage correctedImage = new WritableImage(width, height);
+
+		PixelWriter correctedImageWriter = correctedImage.getPixelWriter();
+		PixelReader imageReader = image.getPixelReader();
+
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				Color pixelColor = imageReader.getColor(x, y);
+
+				int correctedRed = (int) (255 * Math.pow(pixelColor.getRed(), 1.0 / gammaValue));
+				int correctedGreen = (int) (255 * Math.pow(pixelColor.getGreen(), 1.0 / gammaValue));
+				int correctedBlue = (int) (255 * Math.pow(pixelColor.getBlue(), 1.0 / gammaValue));
+
+				Color correctedColor = Color.rgb(correctedRed, correctedGreen, correctedBlue);
+
+				correctedImageWriter.setColor(x, y, correctedColor);
+			}
+		}
+
 		return correctedImage;
 	}
 	
-	public Image gammaCorrecter(Image image, double gammaValue)
-    {
-        int width = (int)image.getWidth();
-        int height = (int)image.getHeight();
-        
-        WritableImage correctedImage = new WritableImage(width, height);
-        
-        PixelWriter correctedImageWriter = correctedImage.getPixelWriter();
-        PixelReader imageReader = image.getPixelReader();
-        
-        for(int y = 0; y < height; y++)
-        {
-            for(int x = 0; x < width; x++)
-            {
-                Color pixelColor = imageReader.getColor(x, y);
-                
-                int correctedRed = (int)(255*Math.pow(pixelColor.getRed(), 1.0/gammaValue));
-                int correctedGreen = (int)(255*Math.pow(pixelColor.getGreen(), 1.0/gammaValue));
-                int correctedBlue = (int)(255*Math.pow(pixelColor.getBlue(), 1.0/gammaValue));
-                
-                Color correctedColor = Color.rgb(correctedRed, correctedGreen, correctedBlue);
-                
-                correctedImageWriter.setColor(x, y, correctedColor);
-            }
-        }
-        
-        return correctedImage;
-    }
-		
-    public static void main(String[] args) 
-    {
-        launch();
-    }
+	public Image contrastStretcher(Image image)
+	{
+		return null;
+	}
+
+	public int getContrastStretchedValue(int colour)
+	{
+		if(colour < r1)
+		{
+			int correctedColour = (int) (((double)s1)/r1 * colour);
+			return correctedColour;
+		}
+		else if(r1 <= colour && colour <= r2)
+		{
+			double multiplier = ((double)(s2 - s1))/(r2 - r1);
+			int correctedColour = (int) (multiplier * (colour - r1) + s1);
+			return correctedColour;
+		}
+		else
+		{
+			double multiplier = ((double)(BYTE_LIMIT - 1 - s2)) / 
+					(BYTE_LIMIT - 1 - r2);
+			int correctedColour = (int) (multiplier * (colour - r2) + s2);
+			return correctedColour;
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		launch();
+	}
 
 }
